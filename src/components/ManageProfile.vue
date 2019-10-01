1c336989226164e24f08b6706329f77b447963ce
baelishSir <sylvain.lerest@viacesi.fr>
2019-10-01 17:51:17 +0200

    add Manage profile page with bootstrap

--- src/components/ManageProfile.vue
+++ src/components/ManageProfile.vue
@@ -1,39 +1,41 @@
 <template>
-<div>
-    <h1>ManageProfile vue</h1>
-    <div>
-        <form action="/action_page.php">
-  Company Name:
-  <input type="text" name="Companyname">
-  <br>
-  First Name:
-  <input type="text" name="firstname">
-  <br>
-  Last Name:
-  <input type="text" name="lastname">
-  <br>
-  Your E-mail:
-  <input type="email" name="youremail">
-  <br>
-  Company E-mail:
-  <input type="email" name="companyemail">
-  <br>
-  Add Company Logo:
-  <input type="image" name="companylogo">
-  <br>
-  Adress/City:
-  <input type="text" name="adresscity">
-  <br>
-  Zip code:
-  <input type="text" name="zip"  pattern="[0-9]">
-  <br>
-  Phone:
-  <input type="tel" name="phone" pattern="[0-9]">
-  <br>
-  <br>
-  <input type="submit" onclick="window.location.href= '#/'" value="Submit">
-</form> 
+
+<div class="container">
+<form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="firstName">First Name:</label>
+      <input type="text" class="form-control" id="firstName" placeholder="First Name">
     </div>
+    <div class="form-group col-md-6">
+      <label for="lastName">Last Name:</label>
+      <input type="text" class="form-control" id="lastName" placeholder="Last Name">
+    </div>
+    <label for="inputEmail">Your Email:</label>
+    <input type="email" class="form-control" id="inputEmail" placeholder="email@gmail.com">
+    <div class="form-group col-md-6">
+      <label for="companyName">Company Name:</label>
+      <input type="text" class="form-control" id="companyName" placeholder="Terretalabs">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputEmailCompany">Company Email:</label>
+      <input type="email" class="form-control" id="inputEmailCompany" placeholder="email@gmail.com">
+    </div>
+    <div class="form-group col-md-10">
+  <label for="inputAddress">Address</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+  </div>
+    <div class="form-group col-md-2">
+      <label for="inputZip">Zip</label>
+      <input type="text" class="form-control" id="inputZip">
+    </div>
+    <div class="form-group">
+      <label for="phone">Phone Number</label>
+      <input type="number" class="form-control" id="phone" placeholder="000000000">
+    </div>
+  </div>
+  
+</form>
 </div>
 </template>
 <script>
