8a43deb0f23c0ed6ebea5751029f79fe1c8af779
Quentin Guisisiano <querzanvarak@gmail.com>
2019-10-02 10:38:09 +0200

    pwa correction

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,16 @@
 <template>
 	<center>
-	<h1>Inspector Gadget</h1>
-	<br>
-	<img src= "../assets/logo.jpg">
-	<br>
-	<br>
-	<button type="button" class="btn btn-primary" onclick="window.location.href= '#/ManageProfile'" >Manage Profile</button>
-	<button type="button" class="btn btn-primary" onclick="window.location.href= '#/MyReports'" >My Reports</button>
+		<div class="container-fluid">
+			<h1>Inspector Gadget</h1>
+			<br>
+			<img src= "../assets/logo.jpg" class="img-fluid">
+			<br>
+			<br>
+			<button type="button" class="btn btn-primary" onclick="window.location.href= '#/ManageProfile'" >Manage Profile</button>
+			<button type="button" class="btn btn-primary" onclick="window.location.href= '#/MyReports'" >My Reports</button>
+		</div>
 	</center>
+
 
 </template>
 <script>
