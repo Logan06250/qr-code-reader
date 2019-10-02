2afaac4721476a7f2c918a149169bcc3214549be
Logan06250 <logan.lamouar@viacesi.fr>
2019-10-02 14:47:45 +0200

    Add feature to add reports

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,8 +7,6 @@
 			<img src= "../assets/logo.jpg" class="img-fluid">
 			<br>
 			<br>
-			<button type="button" class="btn btn-primary" onclick="window.location.href= '#/ManageProfile'" >Manage Profile</button>
-			<button type="button" class="btn btn-primary" onclick="window.location.href= '#/MyReports'" >My Reports</button>
 		</div>
 	</center>
 
--- src/components/MyReports.vue
+++ src/components/MyReports.vue
@@ -1,11 +1,36 @@
 <template>
-<div>
-<h1>MyReports vue</h1>
-</div>
+	<div>
+		<h1>My reports</h1>
+
+		<button id="createNewReport" type="button" class="btn btn-primary" >Add new report</button>
+	</div>
+
+	
+
 </template>
-<script>
+
+
+<script type="javascript">
+	/*document.addEventListener('DOMContentLoaded', function() {
+
+		let reportNb = localStorage.getItem('reportNbElements');
+		if (reportNb == null){
+			localStorage.setItem('reportNbElements', 0)
+			reportNb = 0
+			console.log("Reports number variable initialised.")
+		}
+
+		console.log(reportNb)
+
+		document.getElementById("createNewReport").addEventListener("click", function() {
+			localStorage.setItem('reportNbElements', reportNb++)
+			console.log(reportNb)
+		});
+	});*/
 
 </script>
+
+
 <style>
 
 
