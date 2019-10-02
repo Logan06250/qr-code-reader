98a84fa2c7f2076a3963b247478bf2c25c8b63f0
Logan06250 <logan.lamouar@viacesi.fr>
2019-10-02 11:08:14 +0200

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,50 @@
 <template>
   <div id="app">
-
     <router-view/>
+    <footer>
+      <center>
+        <div class="container-fluid">
+          <form>
+              <input type="button" class="btn btn-primary" value="<-" onclick="history.back()">
+              <button type="button" class="btn btn-primary" onclick="window.location.href= '#/ManageProfile'" >Manage Profile</button>
+              <button type="button" class="btn btn-primary" onclick="window.location.href= '#/MyReports'" >My Reports</button>
+          </form>
+        </div>
+      </center>
+    </footer>
   </div>
 </template>
 <script>
-import Home from './components/Home'
-import ManageProfile from './components/ManageProfile'
-import MyReports from './components/MyReports'
-import PageNotFound from './components/PageNotFound'
-import Report from './components/Report'
+  import Home from './components/Home'
+  import ManageProfile from './components/ManageProfile'
+  import MyReports from './components/MyReports'
+  import PageNotFound from './components/PageNotFound'
+  import Report from './components/Report'
+  import Footer from './components/Footer'
 
-export default {
-  name: 'App',
-  components: {
-    Home,
-    ManageProfile,
-    MyReports,
-    Report,
-    PageNotFound
+  export default {
+    name: 'App',
+    components: {
+      Home,
+      ManageProfile,
+      MyReports,
+      Report,
+      PageNotFound
+    }
   }
-}
 </script>
+
+<style type="text/css">
+  footer{
+
+background:#ffffff;
+
+position:absolute;
+
+bottom:0;
+
+width:100%;
+
+
+}
+</style>
